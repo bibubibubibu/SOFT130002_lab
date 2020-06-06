a8065c39f96029a19ac70a05f0b061e214492684
Dawnsun <40592778+bibubibubibu@users.noreply.github.com>
2020-06-06 11:23:46 +0800

    lab 10

---
+++ lab10/config.php
@@ -0,0 +1,5 @@
+<?php define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'dawnsun');
+define('DBPASS', '1235813');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;'); ?>
